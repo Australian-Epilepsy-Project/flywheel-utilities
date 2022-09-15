eb41046526a39ad6e17be5225eb6e504ed3e10af
Aaron Capon <aaron.capon@florey.edu.au>
2022-09-15 15:44:21 +1000

    Remove fixtures that are no longer used

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,29 +13,3 @@ def acquisition():
     # Information to fill mock acquisition with
     info = {"label": "2-anat-FLAIR_rec-filtered"}
     return Acquisition(**info)
-
-
-@pytest.fixture
-def test_scan(request):
-    ''' Mock Flywheel scan '''
-
-    filename = "sub-101101_ses-101101_echo-5_part-mag_MEGRE.nii.gz"
-    info = {"info": {"BIDS": {"Filename": filename,
-                              "Folder": "anat",
-                              "Path": "sub-101101/ses-101101/anat",
-                              "error_message": "mocked ERROR",
-                              "ignore": request.param['ignore'],
-                              "valid": request.param['valid']}},
-            "type": request.param['type']}
-
-
-    return FileEntry(**info)
-
-
-@pytest.fixture
-def bad_scan():
-    ''' Mock Flywheel scan with no BIDS data'''
-
-    info = {"info": ""}
-
-    return FileEntry(**info)
