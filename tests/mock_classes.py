ef8f1cf7589a5c4479810d2b46d3c581018036ea
Aaron Capon <aaron.capon@florey.edu.au>
2023-01-31 09:38:01 +1100

    Increase coverage for freesurfer.py
    - Increased complexity of mock Context class and added mock Client class

--- tests/mock_classes.py
+++ tests/mock_classes.py
@@ -6,6 +6,7 @@ from pathlib import Path
 
 
 # pylint: disable=too-few-public-methods
+# pylint: disable=too-many-arguments
 # Mock context manager
 class Context:
     """Dummy class docstring"""
@@ -16,9 +17,30 @@ class Context:
         working_dir="",
         gear_name="dummy-name",
         gear_version="3.14.2_0.11.0",
+        fs_license=True,
     ):
         self.config = {"gear-log-level": debug_level}
-
         self.work_dir = Path(working_dir)
-
         self.manifest = {"label": gear_name, "version": gear_version}
+        self.destination = {"id": "tests"}
+        self.fs_license = fs_license
+        self.client = Client(self.fs_license)
+
+
+class Client:
+    """Dummy class docstring"""
+
+    def __init__(self, fs_license: bool = True):
+        self.fs_license = fs_license
+
+    def get_analysis(self, _i):
+        """Mock call"""
+        dummy_dict = {"parents": {"project": "unit_tests"}}
+        return dummy_dict
+
+    def get_project(self, _j):
+        """Mock call"""
+        if self.fs_license:
+            return {"info": {"FREESURFER_LICENSE": "insert_license"}}
+
+        return {"info": {}}
